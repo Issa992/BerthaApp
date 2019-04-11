0707b6440986b29ad6bf9caedf9effcfa0032cd3
Issa <abdu2997@edu.easj.dk>
2019-04-11 11:09:21 +0200

    Testing

--- app/src/androidTest/java/com/example/berthaapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/berthaapp/ExampleInstrumentedTest.java
@@ -2,12 +2,27 @@ package com.example.berthaapp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.service.autofill.Validators.not;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.matcher.PreferenceMatchers.isEnabled;
+import static org.junit.Assert.assertEquals;
+import static android.support.test.espresso.Espresso.onView;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,7 +30,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void ensureTextChanges(){
+        onView(withId(R.id.etName)).perform(typeText("NEw"),closeSoftKeyboard());
+        onView(withId(R.id.etPassword)).perform(typeText("Ty64"),closeSoftKeyboard());
+        onView(withId(R.id.btnLogin)).perform(click());
+
+
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
